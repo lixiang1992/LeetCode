c5ce0ea6cfbf3508dd8ead0c646ab8dfbb762605
lixiang1992 <275005161@qq.com>
2019-04-04 17:27:11 +0800

    284 顶端迭代器

---
+++ myleetCode/src/design/PeekingIterator.java
@@ -0,0 +1,40 @@
+package design;
+
+import java.util.Iterator;
+
+/**
+ * 284.顶端迭代器
+ */
+public class PeekingIterator implements Iterator<Integer> {
+
+    private Integer cache;
+    private Iterator<Integer> iterator;
+    public PeekingIterator(Iterator<Integer> iterator) {
+        this.iterator = iterator;
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    public Integer peek() {
+        if (cache == null && iterator.hasNext()){
+            cache = iterator.next();
+        }
+        return cache;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return cache != null || iterator.hasNext();
+    }
+
+    @Override
+    public Integer next() {
+        if (cache == null){
+            return iterator.next();
+        }else {
+            int temp = cache;
+            cache = null;
+            return temp;
+        }
+    }
+
+}
