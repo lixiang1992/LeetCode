9ffde5850d9251ca125ed0eb5658b416186cf886
lixiang1992 <275005161@qq.com>
2019-07-14 17:35:28 +0800

    1104.二叉树寻路

--- myleetCode/src/tree/binarytree/BinaryTree.java
+++ myleetCode/src/tree/binarytree/BinaryTree.java
@@ -2051,4 +2051,35 @@ public class BinaryTree {
             }
         }
     }
+
+    /**
+     * 1104.二叉树寻路
+     * 在一棵无限的二叉树上，每个节点都有两个子节点，树中的节点 逐行 依次按 “之” 字形进行标记。
+     * 如下图所示，在奇数行（即，第一行、第三行、第五行……）中，按从左到右的顺序进行标记；
+     * 而偶数行（即，第二行、第四行、第六行……）中，按从右到左的顺序进行标记。
+     * 给你树上某一个节点的标号 label，请你返回从根节点到该标号为 label 节点的路径，该路径是由途经的节点标号所组成的。
+     * @param label
+     * @return
+     */
+    public List<Integer> pathInZigZagTree(int label) {
+        LinkedList<Integer> result = new LinkedList<>();
+        int base = 2;
+        int level = log(label,base);// 获取当前层数
+        int value = label;
+        while (level > 0){
+            result.addFirst(value);
+            value = value>>1;// 上一层原本的值直接/2就行了
+            // 值的转换公式是
+            // 对应元素和的公式 = [2^(level+1)+2^level]-1
+            value =  (base+1)*(int)Math.pow(base,level-1) - 1 - value;
+            level--;
+        }
+        result.addFirst(1);
+        return result;
+    }
+
+    private int log(int value,int base){
+        double result = Math.log(value)/Math.log(base);
+        return (int) result;
+    }
 }
