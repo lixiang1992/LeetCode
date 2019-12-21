49554b2da0bcd35d8a33de15b6b922628ea0a871
lixiang1992 <275005161@qq.com>
2019-12-21 23:29:52 +0800

    注释完善

--- myleetCode/src/struct/datastruct/tree/binarytree/KillProcess.java
+++ myleetCode/src/struct/datastruct/tree/binarytree/KillProcess.java
@@ -8,8 +8,14 @@ import java.util.*;
  */
 public class KillProcess {
 
+    /**
+     * key:进程id->value:进程节点的映射缓存
+     */
     private Map<Integer,ProcessNode> m_cache = new HashMap<>();
 
+    /**
+     * 进程节点类
+     */
     private static class ProcessNode{
         int pid;// 进程id
         List<ProcessNode> childList;// 进程的子进程
@@ -18,6 +24,10 @@ public class KillProcess {
             this.pid = pid;
         }
 
+        /**
+         * 进程增加子节点
+         * @param child 子进程
+         */
         void addChild(ProcessNode child){
             if (this.childList == null){
                 this.childList = new LinkedList<>();
@@ -46,8 +56,8 @@ public class KillProcess {
 
     /**
      * 构造进程树
-     * @param pid
-     * @param ppid
+     * @param pid 进程Id
+     * @param ppid 进程父Id
      */
     private void constructProcessTree(List<Integer> pid, List<Integer> ppid){
         // pid是唯一的
@@ -64,9 +74,9 @@ public class KillProcess {
     }
 
     /**
-     * 进程哈希化
-     * @param id
-     * @return
+     * 进程哈希化，记录到缓存中
+     * @param id 进程id
+     * @return 进程节点对象
      */
     private ProcessNode constructHashProcess(Integer id){
         ProcessNode node = m_cache.get(id);
