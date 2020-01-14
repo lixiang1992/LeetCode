963aa3ae934e8b12fd38fdb320392062b7532d76
lixiang1992 <275005161@qq.com>
2020-01-14 14:09:02 +0800

    307.区域和检索 - 数组可修改

--- myleetCode/src/struct/datastruct/tree/segmenttree/NumArray.java
+++ myleetCode/src/struct/datastruct/tree/segmenttree/NumArray.java
@@ -1,94 +1,140 @@
 package struct.datastruct.tree.segmenttree;
 
-import java.util.Deque;
-import java.util.LinkedList;
-
 /**
  * 307.区域和检索 - 数组可修改
+ * 给定一个整数数组  nums，求出数组从索引 i 到 j  (i ≤ j) 范围内元素的总和，包含 i,  j 两点。
+ *
+ * update(i, val) 函数可以通过将下标为 i 的数值更新为 val，从而对数列进行修改。
+ *
+ * 示例:
+ *
+ * Given nums = [1, 3, 5]
+ *
+ * sumRange(0, 2) -> 9
+ * update(1, 2)
+ * sumRange(0, 2) -> 8
+ * 说明:
+ *
+ * 数组仅可以在 update 函数下进行修改。
+ * 你可以假设 update 函数与 sumRange 函数的调用次数是均匀分布的。
+ *
  * 采用segment tree来处理
  */
 public class NumArray {
 
-    private static class SegmentNode {
-        int start;// 线段树节点的区间开始值
-        int end;// 线段树节点的区间结束值
-        int value;
+    // 线段树的root节点
+    private SegmentTree root;
 
-        SegmentNode left;
-        SegmentNode right;
+    /**
+     * 线段树结构体类
+     */
+    private static class SegmentTree {
+        int start;// 节点线段的开始下标
+        int end;// 节点线段的结束下标
+        int sumValue;// 节点线段开始-结束所有值的和，start==end的时候，就是节点本身的值
 
-        SegmentNode(int start, int end) {
+        SegmentTree left;// 左孩子
+        SegmentTree right;// 右孩子
+        SegmentTree parent;// 父亲指针
+
+        SegmentTree(int start,int end){
+            this(start,end,0);
+        }
+
+        SegmentTree(int start,int end,int sumValue){
             this.start = start;
             this.end = end;
-            this.value = 0;
+            this.sumValue = sumValue;
         }
     }
 
-    private SegmentNode root;
-
     public NumArray(int[] nums) {
-        this.root = buildSegmentTreeNode(nums,0,nums.length - 1);
+        this.root = buildSegmentTree(nums,0,nums.length - 1);
     }
 
     /**
-     * 构造线段树节点，递归生成
-     * @param nums
-     * @param start nums[] 的较小下标
-     * @param end nums[] 的较大下标
-     * @return
+     * 构造线段树
+     * @param nums nums
+     * @param start 线段开始位置
+     * @param end 线段结束位置
+     * @return 线段树节点
      */
-    private SegmentNode buildSegmentTreeNode(int nums[],int start,int end){
-        if(start > end){
+    private SegmentTree buildSegmentTree(int[] nums,int start,int end){
+        if (start > end){
             return null;
+        }else if(start == end){
+            // 节点只有一个值了
+            return new SegmentTree(start,end,nums[start]);
         }
-        SegmentNode root = new SegmentNode(start,end);
-        if (start == end){
-            root.value = nums[start];
-            return root;
+        int mid = start + (end - start >> 1);// 取得中间节点
+        // 递归构造左孩子
+        SegmentTree left = buildSegmentTree(nums,start,mid);
+        // 递归构造右孩子
+        SegmentTree right = buildSegmentTree(nums,mid + 1,end);
+        // 当前节点
+        SegmentTree curRoot = new SegmentTree(start,end);
+        // 当前节点的值，等于左右节点之和
+        if (left != null){
+            curRoot.left = left;
+            left.parent = curRoot;
+            curRoot.sumValue += left.sumValue;
         }
-        int mid = start + (end - start >> 1) ;// 中间节点
-        root.left = buildSegmentTreeNode(nums,start,mid);
-        root.right = buildSegmentTreeNode(nums,mid + 1 ,end);
-        if (root.left != null){
-            root.value += root.left.value;
+        if (right != null){
+            curRoot.sumValue += right.sumValue;
+            right.parent = curRoot;
+            curRoot.right = right;
         }
-        if (root.right != null){
-            root.value += root.right.value;
-        }
-        return root;
+        return curRoot;
     }
 
     public void update(int i, int val) {
-        if (i < this.root.start || i>this.root.end){
+        if (i < this.root.start || i > this.root.end){
             return;
         }
-        SegmentNode node = this.root;
-        Deque<SegmentNode> stack = new LinkedList<>();
+        SegmentTree node = this.root;
+        // 想等就是当前节点
         while (node.start != node.end){
-            stack.push(node);// 记录一路走下来的node
-            if (i <= node.left.end){// 这个就是node的mid值
+            int  mid = node.start + (node.end - node.start >> 1);
+            // 左偏移可以等于，右边不行
+            if (mid >= i){
+                // 左偏移
                 node = node.left;
             }else {
+                // 右偏移
                 node = node.right;
             }
         }
-        node.value = val;
-        while (!stack.isEmpty()){
-            node = stack.pop();
-            // 递推更新节点值
-            node.value = node.left.value + node.right.value;
+        // 节点替换
+        node.sumValue = val;
+        // 递推更新父节点
+        while (node.parent != null){
+            node.parent.sumValue = node.parent.left.sumValue + node.parent.right.sumValue;
+            node = node.parent;
         }
     }
 
+    /**
+     * 区间求和
+     * @param i 区间开始下标
+     * @param j 区间结束下标
+     * @return 区间和
+     */
     public int sumRange(int i, int j) {
         return sumNode(root,i,j);
     }
 
-    private int sumNode(SegmentNode root,int i,int j){
+    /**
+     * 区间求和
+     * @param root 当前的线段树节点
+     * @param i 区间开始下标
+     * @param j 区间结束下标
+     * @return 区间和
+     */
+    private int sumNode(SegmentTree root,int i,int j){
         if(root.end == j && root.start == i){
-            return root.value;
+            return root.sumValue;
         }else{
-            int mid = root.start + (root.end-root.start)/2;
+            int mid = root.start + (root.end-root.start >> 1);
             if(mid >= j){
                 return sumNode(root.left,i,j);
             }else if(mid<i){
