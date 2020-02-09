16b3b57fbc6fb0982d534577c312277c4975c086
lixiang1992 <275005161@qq.com>
2020-02-09 23:11:04 +0800

    1244 力扣排行榜

--- myleetCode/src/design/Leaderboard.java
+++ myleetCode/src/design/Leaderboard.java
@@ -1,6 +1,8 @@
 package design;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * 1244.leetcode排行榜
@@ -19,24 +21,70 @@ import java.util.*;
  */
 public class Leaderboard {
 
-    // 维护一个积分排名的数组，保证有序，后面进来的采用二分查找法插入
+    // key 人员 value 分数
     private Map<Integer,Integer> m_rank;
+    // 维护一个积分排名的数组，保证有序，后面进来的采用二分查找法插入，
+    // 用个treeMap相等于直接处理二分了
+    // key 分数 value 同分人数
+    private TreeMap<Integer,Integer> m_score;// 分数
 
     public Leaderboard() {
         m_rank = new HashMap<>();
+        m_score = new TreeMap<>();
     }
 
     public void addScore(int playerId, int score) {
-        m_rank.merge(playerId, score, (a, b) -> a + b);
+        Integer oldScore = m_rank.get(playerId);
+        if (oldScore != null) {
+            // 旧积分人数处理
+            oldScoreNumProcess(oldScore);
+            score += oldScore;
+        }
+        // 选手分数更新
+        m_rank.put(playerId,score);
+        // 分数排名更新
+        m_score.put(score,m_score.getOrDefault(score,0) + 1);
+    }
+
+    /**
+     * 旧积分人数的处理
+     * @param score 积分
+     */
+    private void oldScoreNumProcess(int score){
+        int scoreNum = m_score.get(score);
+        if (scoreNum <= 1){
+            // 该积分没有人了，积分移除
+            m_score.remove(score);
+        } else {
+            // 该积分人数减一
+            m_score.put(score,scoreNum - 1);
+        }
     }
 
     public int top(int K) {
-
-        return 0;
+        int sumScore = 0;
+        Map.Entry<Integer,Integer> scoreEntry = m_score.lastEntry();
+        while (K > 0 && scoreEntry != null){
+            // key 分数 value 人数
+            // 积分人数如果大于K了，取K个人，否则全部取出
+            int num = Math.min(scoreEntry.getValue(),K);
+            // 分数*数量
+            sumScore += scoreEntry.getKey() * num;
+            // 分数前移，找小的
+            scoreEntry = m_score.lowerEntry(scoreEntry.getKey());
+            K-= num;
+        }
+        return sumScore;
     }
 
     public void reset(int playerId) {
-        // 积分清零
+        // 选手积分
+        int score = m_rank.get(playerId);
+        // 选手积分清零
         m_rank.put(playerId,0);
+        // 积分人数处理
+        oldScoreNumProcess(score);
+        // 0分人数+1
+        m_score.put(0,m_score.getOrDefault(0,0) + 1);
     }
 }
