f82c78567338b0775af994a7974cde0474120011
lixiang1992 <275005161@qq.com>
2020-02-14 20:15:59 +0800

    二叉搜索树的后序遍历序列

--- myleetCode/src/struct/datastruct/tree/binarysearchtree/BinarySearchTree.java
+++ myleetCode/src/struct/datastruct/tree/binarysearchtree/BinarySearchTree.java
@@ -393,6 +393,70 @@ public class BinarySearchTree {
     }
 
     /**
+     * 255.验证前序遍历序列二叉搜索树
+     * 单调栈->单调递减栈存储preorder的思路
+     * 二叉搜索树是left < root < right的，先序遍历又是root->left->right的，基于这样的性质和遍历方式，我们知道越往左越小，这样，就可以构造一个单调递减的栈，来记录遍历的元素。
+     *
+     * 为什么要用单调栈呢，因为往左子树遍历的过程，value是越来越小的，一旦出现了value大于栈顶元素value的时候，就表示要开始进入右子树了（如果不是，就应该继续进入左子树，不理解的请看下二叉搜索树的定义），但是这个右子树是从哪个节点开始的呢？
+     *
+     * 单调栈帮我们记录了这些节点，只要栈顶元素还比当前节点小，就表示还是左子树，要移除，因为我们要找到这个右孩子节点直接连接的父节点，也就是找到这个子树的根，只要栈顶元素还小于当前节点，就要一直弹出，直到栈顶元素大于节点，或者栈为空。栈顶的上一个元素就是子树节点的根。
+     *
+     * 接下来，数组继续往后遍历，之后的右子树的每个节点，都要比子树的根要大，才能满足，否则就不是二叉搜索树
+     *
+     * @param preorder 先序遍历数组
+     * @return 是否是先序遍历二叉搜索树
+     */
+    public boolean verifyPreorder(int[] preorder) {
+        // 单调栈使用，单调递减的单调栈
+        Deque<Integer> stack = new LinkedList<>();
+        int prevElement = Integer.MIN_VALUE;
+        for (int i = 0;i<preorder.length;i++){
+            // 右子树元素必须要大于递减栈被peek访问的元素，否则就不是二叉搜索树
+            if (preorder[i] < prevElement){
+                return false;
+            }
+            while (!stack.isEmpty() && preorder[i] > stack.peek()){
+                // 数组元素大于单调栈的元素了，表示往右子树走了，记录下上个根节点
+                // 找到这个右子树对应的根节点，之前左子树全部弹出，不再记录，因为不可能在往根节点的左子树走了
+                prevElement = stack.pop();
+            }
+            // 这个新元素入栈
+            stack.push(preorder[i]);
+        }
+        return true;
+    }
+
+    /**
+     * 二叉搜索树的后序遍历序列
+     * 同255.验证前序遍历序列二叉搜索树，
+     * 后序遍历由left->right->root转化为root->right->left的翻转先序遍历
+     * 构造一个单调递增的单调栈，因为right>root>left
+     *
+     * @param postorder 有序遍历数组
+     * @return 是否后序遍历二叉搜索树
+     */
+    public boolean verifyPostorder(int[] postorder) {
+        // 单调栈使用，单调递增的单调栈
+        Deque<Integer> stack = new LinkedList<>();
+        int pervElem = Integer.MAX_VALUE;
+        // 逆向遍历，就是翻转的先序遍历
+        for (int i = postorder.length - 1;i>=0;i--){
+            // 左子树元素必须要小于递增栈被peek访问的元素，否则就不是二叉搜索树
+            if (postorder[i] > pervElem){
+                return false;
+            }
+            while (!stack.isEmpty() && postorder[i] < stack.peek()){
+                // 数组元素小于单调栈的元素了，表示往左子树走了，记录下上个根节点
+                // 找到这个左子树对应的根节点，之前右子树全部弹出，不再记录，因为不可能在往根节点的右子树走了
+                pervElem = stack.pop();
+            }
+            // 这个新元素入栈
+            stack.push(postorder[i]);
+        }
+        return true;
+    }
+
+    /**
      * 270. 最接近的二叉搜索树值
      * 其实完全可以把target看成是在TreeNode中要查找的点
      * target如果大于root.val，那么最接近target的值，肯定在root.right的子树中，我们只要进入右子树去查找就行
@@ -1066,6 +1130,11 @@ public class BinarySearchTree {
 
     /**
      * 975.奇偶跳
+     *
+     * 奇数次跳跃时，在右边找比它大的数中最小的数
+     * 偶数次跳跃时，在右边找比它小的树中最大的数
+     * TreeMap  刚好有满足上面数据结构的方法
+     *
      * @param A
      * @return
      */
