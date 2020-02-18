2baaeb1dc0cd9cc1cf3963a677691dc472d1214a
lixiang1992 <275005161@qq.com>
2020-02-18 18:08:53 +0800

    思路描述完善

--- myleetCode/src/struct/datastruct/tree/binarytree/PathSum_437.java
+++ myleetCode/src/struct/datastruct/tree/binarytree/PathSum_437.java
@@ -15,7 +15,7 @@ import java.util.Map;
  *
  * 二叉树不超过1000个节点，且节点数值范围是 [-1000000,1000000] 的整数。
  *
- * 前缀和的递归回溯思路
+ * 前缀和的递归回溯思路，如果此前有和为currSum-target,而当前的和又为currSum,两者的差就肯定为target了
  */
 public class PathSum_437 {
 
@@ -30,7 +30,8 @@ public class PathSum_437 {
 
     /**
      * 前缀和的递归回溯思路
-     * 从当前节点反推到根节点，有且仅有一条路径，因为这是一棵树
+     * 从当前节点反推到根节点(反推比较好理解，正向其实也只有一条)，有且仅有一条路径，因为这是一棵树
+     * 如果此前有和为currSum-target,而当前的和又为currSum,两者的差就肯定为target了
      * 所以前缀和对于当前路径来说是唯一的，当前记录的前缀和，在回溯结束，回到本层时去除，保证其不影响其他分支的结果
      * @param node 树节点
      * @param prefixSumCount 前缀和Map
@@ -50,8 +51,8 @@ public class PathSum_437 {
 
         //---核心代码
         // 先看看root到当前节点这条路上是否存在节点前缀和加target为currSum的路径
-        // 当前节点->root节点反推，有且仅有一条路径
-        // currSum-target等于从这个点反推回去，减掉target的值的前缀点，能找到几个这样的前缀和的点
+        // 当前节点->root节点反推，有且仅有一条路径，如果此前有和为currSum-target,而当前的和又为currSum,两者的差就肯定为target了
+        // currSum-target相当于找路径的起点，起点的sum+target=currSum，当前点到起点的距离就是target
         res += prefixSumCount.getOrDefault(currSum - target, 0);
         // 更新路径上当前节点前缀和的个数
         prefixSumCount.put(currSum, prefixSumCount.getOrDefault(currSum, 0) + 1);
