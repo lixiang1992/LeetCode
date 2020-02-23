b1c05427e68b10353c9eaecbed84f6858e9b9354
lixiang1992 <275005161@qq.com>
2020-02-23 10:04:01 +0800

    560.和为K的整数

---
+++ myleetCode/src/hash/SubarraySum_560.java
@@ -0,0 +1,41 @@
+package hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 560. 和为K的子数组
+ * 给定一个整数数组和一个整数 k，你需要找到该数组中和为 k 的连续的子数组的个数。
+ * <p>
+ * 示例 1 :
+ * <p>
+ * 输入:nums = [1,1,1], k = 2
+ * 输出: 2 , [1,1] 与 [1,1] 为两种不同的情况。
+ * 说明 :
+ * <p>
+ * 数组的长度为 [1, 20,000]。
+ * 数组中元素的范围是 [-1000, 1000] ，且整数 k 的范围是 [-1e7, 1e7]。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/subarray-sum-equals-k
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class SubarraySum_560 {
+
+    // 与437题一样，采用前缀和的思路
+    public int subarraySum(int[] nums, int k) {
+        // key 前缀和 value : 前缀和出现的次数
+        Map<Integer, Integer> prefixSumCount = new HashMap<>();
+        prefixSumCount.put(0, 1);
+        int sum = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            // 前缀和的数量累加。如果此前有和为sum-k,而当前的和又为sum,两者的差就肯定为k了
+            count += prefixSumCount.getOrDefault(sum - k, 0);
+            // 更新前缀和数量的累加
+            prefixSumCount.put(sum, prefixSumCount.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+}
