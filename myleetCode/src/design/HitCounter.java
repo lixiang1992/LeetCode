0ffb89152cfc4620827de4a9a36a4d33f51c9e77
lixiang1992 <275005161@qq.com>
2020-02-29 10:42:49 +0800

    326.敲击计数器

--- myleetCode/src/design/HitCounter.java
+++ myleetCode/src/design/HitCounter.java
@@ -15,9 +15,9 @@ import java.util.TreeMap;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class HitCounter {
+
     // key 敲击时刻 value 在这一时刻之前的闭区间计数之和
     private TreeMap<Integer,Integer> m_hitCountMap;// 敲击计数treeMap
-    private int m_lastHitTime = 0;// 上一次的敲击时刻
 
     /** Initialize your data structure here. */
     public HitCounter() {
@@ -29,22 +29,21 @@ public class HitCounter {
     public void hit(int timestamp) {
         // 把上一次敲击时间次数累加到这一次，运用前缀和的思想
         // 因为timestamp是递增的，所以上次的时间一定<=这次时间
-        m_hitCountMap.put(timestamp,m_hitCountMap.getOrDefault(m_lastHitTime,0) + 1);
-        m_lastHitTime = timestamp;
+        Map.Entry<Integer,Integer> lastHitEntry = m_hitCountMap.floorEntry(timestamp);// 上次敲击时刻
+        int lastHit = lastHitEntry == null ? 0 : lastHitEntry.getValue();// 之前敲击次数之和
+        m_hitCountMap.put(timestamp,lastHit + 1);// 前缀和累加
     }
 
     /** Return the number of hits in the past 5 minutes.
      @param timestamp - The current timestamp (in seconds granularity). */
     public int getHits(int timestamp) {
-        if (m_lastHitTime <= 0){
-            return 0;// 不存在上次的敲击时刻，直接返回0
-        }
         int minTime = timestamp - 300;
         // 5分钟前的时刻，利用前缀和获取敲击时间<=minTime的最接近时刻
         Map.Entry<Integer,Integer> minEntry = m_hitCountMap.floorEntry(minTime);
         // 上一次敲击的时刻
-        int max = m_hitCountMap.get(m_lastHitTime);
+        Map.Entry<Integer,Integer> maxEntry = m_hitCountMap.floorEntry(timestamp);
         int min = minEntry == null ? 0 : minEntry.getValue();
+        int max = maxEntry == null ? 0 : maxEntry.getValue();
         // 两者之差就是这个时间范围内的敲击次数
         return max - min;
     }
