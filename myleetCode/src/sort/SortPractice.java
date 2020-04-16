62104277430f6c77b8daeab29d30838d6d6fe635
lixiang1992 <275005161@qq.com>
2020-04-17 06:59:01 +0800

    56.合并区间

--- myleetCode/src/sort/SortPractice.java
+++ myleetCode/src/sort/SortPractice.java
@@ -2,11 +2,45 @@ package sort;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Deque;
+import java.util.LinkedList;
 
 /**
  * 排序练习
  */
 public class SortPractice {
+
+    /**
+     * 56.合并区间
+     * @param intervals
+     * @return
+     */
+    public int[][] merge(int[][] intervals) {
+        Deque<int[]> queue = new LinkedList<>();
+        Arrays.sort(intervals, (v1, v2) -> v1[0] - v2[0]);
+        // 遍历
+        for (int[] current : intervals) {
+            if (queue.size() > 0){
+                // 上一个元素
+                int[] last = queue.peekLast();
+                if (last[1] >= current[0]){
+                    // 区间发生了重叠
+                  last[1] = Math.max(last[1],current[1]);
+                } else {
+                    // 区间没有重叠
+                    queue.offer(current);
+                }
+            }else{
+                queue.offer(current);
+            }
+        }
+        int[][] res = new int[queue.size()][2];
+        int i = 0;
+        while (!queue.isEmpty()){
+            res[i++] = queue.poll();
+        }
+        return res;
+    }
 
     private class LargestComparator implements Comparator<String> {
 
@@ -34,9 +68,24 @@ public class SortPractice {
             return "0";
         }
         StringBuilder result = new StringBuilder();
-        for (int i=0;i<strArray.length;i++){
-            result.append(strArray[i]);
+        for (String s : strArray) {
+            result.append(s);
         }
         return result.toString();
+    }
+
+    /**
+     * 378. 有序矩阵中第K小的元素
+     * N个有序数组，归并排序
+     * @param matrix
+     * @param k
+     * @return
+     */
+    public int kthSmallest(int[][] matrix, int k) {
+        int[] res = matrix[0].clone();
+        for (int i = 1; i < matrix.length; i++) {
+
+        }
+        return 0;
     }
 }
