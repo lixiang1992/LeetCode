1d59d5943cf28a39de6712dded9a9dad180194d6
lixiang1992 <275005161@qq.com>
2020-04-20 22:50:07 +0800

    岛屿问题

---
+++ myleetCode/src/struct/datastruct/graph/dfs/NumIslands_200.java
@@ -0,0 +1,38 @@
+package struct.datastruct.graph.dfs;
+
+/**
+ * 200.岛屿数量
+ * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ * <p>
+ * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+ * <p>
+ * 此外，你可以假设该网格的四条边均被水包围。
+ */
+public class NumIslands_200 {
+
+    // 沉岛思想
+    public int numIslands(char[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(i, j, grid);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    private void dfs(int i, int j, char[][] grid) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
+            return ;
+        }
+        grid[i][j] = '0';
+        dfs(i - 1, j, grid);
+        dfs(i + 1, j, grid);
+        dfs(i, j - 1, grid);
+        dfs(i, j + 1, grid);
+    }
+
+}
